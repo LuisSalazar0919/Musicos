0e20bae9056e8c3d7c3fb322e1b141f6b2043713
LuisSalazar0919 <126519399+LuisSalazar0919@users.noreply.github.com>
2023-03-22 19:59:59 +0000

    codigo

---
+++ Musicos/Program.cs
@@ -0,0 +1,76 @@
+﻿class Musico
+{
+public string Nombre {get; set;}
+
+
+
+public Musico (string No)
+{
+    Nombre= No;
+
+}
+
+public void saluda()
+{
+    Console.WriteLine("hola buenas noches");
+}
+
+public virtual void Tocar()
+{
+    Console.WriteLine($"{Nombre} esta tocando su instrumento");
+}
+
+
+}
+
+
+
+
+
+class Baterista:Musico
+{
+    public string Bateria {get; set;}
+
+    public Baterista(string No, string bateria):base(No)
+    {
+        Bateria = bateria;
+    }
+
+    public /*override*/ new void Tocar()
+    {
+        Console.WriteLine($"{Nombre} esta tocando su {Bateria}");
+    }
+}
+
+class Bajista:Musico
+{
+    public string Bajo {get; set;}
+
+    public Bajista(string No, string bajo):base(No)
+    {
+        this.Bajo = bajo;
+    }
+
+    public /*override*/new  void Tocar()
+    {
+        Console.WriteLine($"{Nombre} tocando su {Bajo}");
+    }
+
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Musico> grupoPerron = new List<Musico>();
+        grupoPerron.Add(new Bajista ("ronaldo", "sexto"));
+        grupoPerron.Add(new Baterista("pedro","la normal"));
+        grupoPerron.Add(new Musico("Bon jovi"));
+
+        foreach (var m in grupoPerron) m.saluda();
+        foreach (var m in grupoPerron) m.Tocar();
+
+
+    }
+}
