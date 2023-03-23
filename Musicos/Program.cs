faa0f492d1168da876b912a1a249a8fb2264966c
LuisSalazar0919 <126519399+LuisSalazar0919@users.noreply.github.com>
2023-03-23 19:34:44 +0000

    clase abstracta

--- Musicos/Program.cs
+++ Musicos/Program.cs
@@ -1,4 +1,4 @@
-﻿class Musico
+﻿abstract class Musico
 {
 public string Nombre {get; set;}
 
@@ -15,15 +15,13 @@ public void saluda()
     Console.WriteLine("hola buenas noches");
 }
 
-public virtual void Tocar()
-{
-    Console.WriteLine($"{Nombre} esta tocando su instrumento");
+public /* virtual*/ abstract void Tocar();
+
+    /*Console.WriteLine($"{Nombre} esta tocando su instrumento");*/
+
+
+
 }
-
-
-}
-
-
 
 
 
@@ -36,7 +34,7 @@ class Baterista:Musico
         Bateria = bateria;
     }
 
-    public /*override*/ new void Tocar()
+    public override void Tocar()
     {
         Console.WriteLine($"{Nombre} esta tocando su {Bateria}");
     }
@@ -51,7 +49,7 @@ class Bajista:Musico
         this.Bajo = bajo;
     }
 
-    public /*override*/new  void Tocar()
+    public override  void Tocar()
     {
         Console.WriteLine($"{Nombre} tocando su {Bajo}");
     }
@@ -66,7 +64,7 @@ internal class Program
         List<Musico> grupoPerron = new List<Musico>();
         grupoPerron.Add(new Bajista ("ronaldo", "sexto"));
         grupoPerron.Add(new Baterista("pedro","la normal"));
-        grupoPerron.Add(new Musico("Bon jovi"));
+       /* grupoPerron.Add(new Musico("Bon jovi"));*/
 
         foreach (var m in grupoPerron) m.saluda();
         foreach (var m in grupoPerron) m.Tocar();
